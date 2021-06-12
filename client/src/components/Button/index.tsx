7e5cfc75381c3adc1dc5e6100ee392cbf718fd6c
lyndsiWilliams <kcatgirl@gmail.com>
2021-06-12 01:08:55 -0500

    Added ellipses library and created type for it

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -3,6 +3,7 @@ import { Button as AntdButton } from 'antd';
 import { ButtonProps as AntdButtonProps } from 'antd/lib/button';
 import styled from '@emotion/styled';
 import 'antd/dist/antd.css';
+import LinesEllipsis from 'react-lines-ellipsis';
 
 export interface ButtonProps extends AntdButtonProps {
   buttonText?: string;
@@ -13,13 +14,21 @@ const Button = styled(
     <AntdButton
       {...props}
     >
-      {buttonText}
+      <LinesEllipsis
+        text={buttonText}
+        maxLine='1'
+        basedOn='letters'
+        trimRight
+      />
     </AntdButton>
   ),
 )`
   background-color: rebeccapurple;
   color: lavender;
   border: 1px solid lavender;
+  overflow: hidden;
+  white-space: break-spaces;
+  width: 100%;
 
   &:hover, &:focus {
     background-color: lavender;
