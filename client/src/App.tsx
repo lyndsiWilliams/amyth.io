76926b8d74b2e709e7126828428bb767bad76371
Lyndsi Kay Williams <55605634+lyndsiWilliams@users.noreply.github.com>
2021-06-13 01:34:19 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -47,7 +47,7 @@ function App() {
         />
         <Button
           block
-          href='https://60bafb54de11ec00490290c8-plhadeupxh.chromatic.com/'
+          href='https://60bafb54de11ec00490290c8-gsgpwgexgq.chromatic.com/'
           target='_blank'
           rel='noopener noreferrer'
         >
