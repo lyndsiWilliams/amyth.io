b5f0995c6e5ba70829468db70ae0086a370e885d
lyndsiWilliams <kcatgirl@gmail.com>
2021-06-24 16:07:35 -0600

    Bb AmyCard Storybook

---
+++ client/src/components/AmythicalCard/AmythicalCard.stories.tsx
@@ -0,0 +1,17 @@
+import AmythicalCard from '.';
+import { CardProps } from 'antd/lib/card';
+
+export default {
+  title: 'Card',
+  component: AmythicalCard,
+};
+
+export const InteractiveAmythicalCard = (args: CardProps) => {
+  return (
+    <AmythicalCard title={args.title}>Test</AmythicalCard>
+  );
+};
+
+InteractiveAmythicalCard.args = {
+  title: "Card title",
+};
